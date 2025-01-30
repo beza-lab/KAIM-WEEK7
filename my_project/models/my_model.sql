17349bd6043ef5e0301554ec1c06153e38727135
Bezawit Girma <gahebs@gmail.com>
2025-01-30 14:09:02 +0300

    add DBT for Data Transformation my project

---
+++ my_project/models/my_model.sql
@@ -0,0 +1,10 @@
+
+  create view "telegram_data"."cleaned_data"."my_model__dbt_tmp"
+    
+    
+  as (
+    -- models/my_model.sql
+SELECT *
+FROM "telegram_data"."public"."cleaned_data"
+WHERE "Channel" = 'doctorset'
+  );
