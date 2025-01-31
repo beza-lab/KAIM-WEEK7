a1c5f5eb52e4b1d6ae02549a5f9cd473beeb8c8d
Bezawit Girma <gahebs@gmail.com>
2025-01-31 10:45:37 +0300

    add DBT my_model.sql sources.yml and other files

--- my_project/models/my_model.sql
+++ my_project/models/my_model.sql
@@ -1,10 +1,9 @@
-
-  create view "telegram_data"."cleaned_data"."my_model__dbt_tmp"
-    
-    
-  as (
-    -- models/my_model.sql
-SELECT *
-FROM "telegram_data"."public"."cleaned_data"
-WHERE "Channel" = 'doctorset'
+create view "telegram_data"."cleaned_data"."my_model__dbt_tmp"
+    
+    
+  as (
+    -- models/my_model.sql
+SELECT *
+FROM "telegram_data"."public"."cleaned_data"
+WHERE "Channel" = 'doctorset'
   );
